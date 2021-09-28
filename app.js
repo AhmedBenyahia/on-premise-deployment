5efb0aa6417fa5201a63205b3a0268eba4397a26
Ahmed-Benyahia <ahmedbenyahiass@gmail.com>
2021-09-28 11:58:35 +0100

    Add build command for spring boot project

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const router = express.Router();
 const app = express();
 const debug = require('debug')('on-premise-deployment:server');
 const shell = require('shelljs');
+const config = require('config');
 
 const runBuild = () => {
     if (!shell.which('git')) {
         shell.echo('Sorry, this script requires git');
         shell.exit(1);
     }
+    !shell.cd(config.get("repoPath")).code &&
+    !shell.exec('git pull').code &&
+    !shell.exec('mvn clean install -DskipTests').code &&
+    !shell.exec('sudo docker build -t ltm-api:1.13 .').code &&
+    !shell.exec('sudo docker stop ltm-api').code &&
+    shell.exec('sudo docker run -p 5005:5005 -p 3306:306 --name ltm-api ltm-api:1.13');
 }
 
 app.use(logger('dev'));
@@ -23,6 +30,7 @@ app.use("/",router);
 /* POST webhook. */
 router.post('/', async (req, res, next) => {
     debug(req.body)
+    runBuild()
     res.send("OK");
 });
 

9f9f5c7b55294620657a857e20edcb74a3f79b7d
Ahmed-Benyahia <ahmedbenyahiass@gmail.com>
2021-09-28 12:22:02 +0100

    Stop app server before running the build

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const debug = require('debug')('on-premise-deployment:server');
 const shell = require('shelljs');
 const config = require('config');
 
-const runBuild = () => {
+const runBuild = async () => {
     if (!shell.which('git')) {
         shell.echo('Sorry, this script requires git');
         shell.exit(1);
@@ -15,9 +15,12 @@ const runBuild = () => {
     !shell.exec('git pull').code &&
     !shell.exec('mvn clean install -DskipTests').code &&
     !shell.exec('sudo docker build -t ltm-api:1.13 .').code &&
-    !shell.exec('sudo docker stop ltm-api').code &&
-    !shell.exec('sudo docker rm ltm-api').code &&
     shell.exec('sudo docker run -p 5005:5005 -p 3306:306 --name ltm-api ltm-api:1.13');
+}
+
+const stopAppServer = () => {
+    !shell.exec('sudo docker stop ltm-api').code &&
+    shell.exec('sudo docker rm ltm-api')
 }
 
 app.use(logger('dev'));
@@ -31,6 +34,7 @@ app.use("/",router);
 /* POST webhook. */
 router.post('/', async (req, res, next) => {
     debug(req.body)
+    stopAppServer()
     runBuild()
     res.send("OK");
 });
